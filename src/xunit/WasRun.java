57da1363b222b89ab2d68f81cc9bf489cf9a347f
machntek <machntek@gmail.com>
2021-10-25 23:13:31 +0900

    feat: setUp 메소드 완성

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -5,13 +5,18 @@ import java.lang.reflect.Method;
 
 public class WasRun extends TestCase {
     public boolean wasRun;
+    public boolean wasSetUp;
 
     public WasRun(String name) {
         super(name);
     }
 
+    @Override
+    public void setUp() {
+        wasSetUp = true;
+    }
+
     public void testMethod() {
         wasRun = true;
     }
-
 }
