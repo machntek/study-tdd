4e9d6e16e4fd5524bca5fc936fcf18e01aa1e850
machntek <machntek@gmail.com>
2021-10-25 22:52:28 +0900

    initial commit

---
+++ src/xunit/WasRun.java
@@ -0,0 +1,17 @@
+package xunit;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class WasRun extends TestCase {
+    public boolean wasRun;
+
+    public WasRun(String name) {
+        super(name);
+    }
+
+    public void testMethod() {
+        wasRun = true;
+    }
+
+}
